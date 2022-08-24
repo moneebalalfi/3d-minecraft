74f16c347e82d946002f0a5ddc5e337a2f2132b2
moneeb <moneebalalfi@gmail.com>
2022-08-25 01:52:47 +0300

    chore: setting up the project;

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { sRGBEncoding } from "three";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      gl={{
+        outputEncoding: sRGBEncoding,
+      }}
+    >
+      <mesh>
+        <Box />
+      </mesh>
+    </Canvas>
   );
 }
 
